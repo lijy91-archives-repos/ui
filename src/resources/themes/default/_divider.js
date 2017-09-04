29a0f7515a0919f674ab6e6942107d2e59abc199
JianyingLi <lijy91@foxmail.com>
2017-09-05 00:07:15 +0800

    Fix divider not show in Safari

--- src/resources/themes/default/_divider.js
+++ src/resources/themes/default/_divider.js
@@ -4,11 +4,13 @@ export default {
     backgroundColor: '#dddddd',
   },
   'Divider[orientation=vertical]': {
+    minWidth: 1,
     minHeight: 20,
     width: 1,
   },
   'Divider[orientation=horizontal]': {
     minWidth: 20,
+    minHeight: 1,
     height: 1,
   },
 };
