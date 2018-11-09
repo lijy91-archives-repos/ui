193ceca762dc98df03e5ab4385e2c2fd3bfd591c
JianyingLi <lijy91@foxmail.com>
2018-11-09 09:15:48 +0800

    Remove barStyle in Screen

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,35 +1,4 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { StatusBar, View as RNView } from 'react-native';
-
+import { View as RNView } from 'react-native';
 import withStyles from '../../withStyles';
 
-const propTypes = {
-  barStyle: PropTypes.string,
-};
-
-const defaultProps = {
-  barStyle: 'dark-content',
-};
-
-class Screen extends PureComponent {
-  render() {
-    const {
-      barStyle,
-      children,
-    } = this.props;
-    return (
-      <RNView
-        {...this.props}
-      >
-        <StatusBar barStyle={barStyle} />
-        {children}
-      </RNView>
-    );
-  }
-}
-
-Screen.propTypes = propTypes;
-Screen.defaultProps = defaultProps;
-
-export default withStyles('Screen')(Screen);
+export default withStyles('Screen')(RNView);
